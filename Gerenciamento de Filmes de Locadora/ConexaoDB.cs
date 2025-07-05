69a65627f2749746eac773c536675a62b1f5bb11
Kayo Weiber <caioveiber598@gmail.com>
2025-07-05 17:43:35 -0300

    feat(conexaoDB) - adicionado conexao de baco de dados e criações das pastas iniciais

---
+++ Gerenciamento de Filmes de Locadora/ConexaoDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Gerenciamento_de_Filmes_de_Locadora
+{
+    internal class ConexaoDB
+    {
+        public static MySqlConnection fazconexao()
+        {
+            return new MySqlConnection("server=localhost;port=3306;database=db_locadora;uid=root;pwd=123456789;"); //removi a senha "pwd";
+        }
+    }
+}
