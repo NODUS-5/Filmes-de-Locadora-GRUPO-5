ab7569898a09227b857c77c404d0200e5084a0bf
Kayo Weiber <caioveiber598@gmail.com>
2025-07-05 17:46:23 -0300

    fix(conexaoDB): remoção do comentario

--- Gerenciamento de Filmes de Locadora/ConexaoDB.cs
+++ Gerenciamento de Filmes de Locadora/ConexaoDB.cs
@@ -12,7 +12,7 @@ namespace Gerenciamento_de_Filmes_de_Locadora
     {
         public static MySqlConnection fazconexao()
         {
-            return new MySqlConnection("server=localhost;port=3306;database=db_locadora;uid=root;pwd=123456789;"); //removi a senha "pwd";
+            return new MySqlConnection("server=localhost;port=3306;database=db_locadora;uid=root;pwd=123456789;");
         }
     }
 }
