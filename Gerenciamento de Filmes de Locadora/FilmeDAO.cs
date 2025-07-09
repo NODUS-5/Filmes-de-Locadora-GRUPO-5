29fe67cda5b164a4b0decb1be84d6c15fe059dcc
Kayo Weiber <caioveiber598@gmail.com>
2025-07-09 20:13:09 -0300

    feat(FilmeDAO): Criado a classe FilmeDAO e o método "InserirFilme"
    - Método InserirFilme necessita ser passado todo o conteudo do insert.

---
+++ Gerenciamento de Filmes de Locadora/FilmeDAO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Gerenciamento_de_Filmes_de_Locadora
+{
+    public class FilmeDAO
+    {
+        public static void InserirFilme(string titulo, string diretor,string genero,int anoLancamento,string classiIndicativa,int duracaoMinutos) //método para inserir o filme recebendo todos os parâmetros necessários
+        {
+            MySqlConnection cn = ConexaoDB.fazconexao();
+            try
+            {
+                cn.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "INSERT INTO filmes (titulo, diretor, genero, ano_lancamento, classificacao_indicativa, duracao_minutos) VALUES ('"+titulo+"','"+diretor+"','"+genero+ "',"+anoLancamento+ ",'"+classiIndicativa+ "',"+duracaoMinutos+")";
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                Console.WriteLine("Filme inserido com sucesso!!");
+
+
+            }catch (MySqlException ex)
+            {
+                Console.WriteLine("Impossível conectar.\n" + ex.Message);
+            }
+        }
+
+    }
+}
